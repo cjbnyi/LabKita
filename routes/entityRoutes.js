ee981d7173f1ba508b95b8fc8392ea8600e03255
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2025-03-11 01:50:05 +0800

    feat: update middleware

--- routes/entityRoutes.js
+++ routes/entityRoutes.js
@@ -21,39 +21,39 @@ const router = express.Router();
 /* =============================== */
 router.get('/admins', checkValidation, adminController.getAdmins);
 router.post('/admins', ...validateUserFields(), checkValidation, adminController.createAdmin);
-router.put('/admins/:adminId', validateMongoId, ...validateUserFields(), checkValidation, adminController.updateAdmin);
-router.delete('/admins/:adminId', validateMongoId, checkValidation, adminController.deleteAdmin);
+router.put('/admins/:adminId', validateMongoId('adminId'), ...validateUserFields(), checkValidation, adminController.updateAdmin);
+router.delete('/admins/:adminId', validateMongoId('adminId'), checkValidation, adminController.deleteAdmin);
 
 /* =============================== */
 /* STUDENT */
 /* =============================== */
 router.get('/students', checkValidation, studentController.getStudents);
 router.post('/students', ...validateUserFields(), checkValidation, studentController.createStudent);
-router.put('/students/:studentId', validateMongoId, ...validateUserFields(), checkValidation, studentController.updateStudent);
-router.delete('/students/:studentId', validateMongoId, checkValidation, studentController.deleteStudent);
+router.put('/students/:studentId', validateMongoId('studentId'), ...validateUserFields(), checkValidation, studentController.updateStudent);
+router.delete('/students/:studentId', validateMongoId('studentId'), checkValidation, studentController.deleteStudent);
 
 /* =============================== */
 /* LAB */
 /* =============================== */
 router.get('/labs', checkValidation, labController.getLabs);
 router.post('/labs', checkValidation, labController.createLab);
-router.put('/labs/:labId', validateMongoId, checkValidation, labController.updateLab);
-router.delete('/labs/:labId', validateMongoId, checkValidation, labController.deleteLab);
+router.put('/labs/:labId', validateMongoId('labId'), checkValidation, labController.updateLab);
+router.delete('/labs/:labId', validateMongoId('labId'), checkValidation, labController.deleteLab);
 
 /* =============================== */
 /* SEAT */
 /* =============================== */
 router.get('/seats', checkValidation, seatController.getSeats);
 router.post('/seats', checkValidation, seatController.createSeat);
-router.put('/seats/:seatId', validateMongoId, checkValidation, seatController.updateSeat);
-router.delete('/seats/:seatId', validateMongoId, checkValidation, seatController.deleteSeat);
+router.put('/seats/:seatId', validateMongoId('seatId'), checkValidation, seatController.updateSeat);
+router.delete('/seats/:seatId', validateMongoId('seatId'), checkValidation, seatController.deleteSeat);
 
 /* =============================== */
 /* RESERVATION */
 /* =============================== */
 router.get('/reservations', checkValidation, reservationController.getReservations);
 router.post('/reservations', checkValidation, reservationController.createReservation);
-router.put('/reservations/:reservationId', validateMongoId, checkValidation, reservationController.updateReservation);
-router.delete('/reservations/:reservationId', validateMongoId, checkValidation, reservationController.deleteReservation);
+router.put('/reservations/:reservationId', validateMongoId('reservationId'), checkValidation, reservationController.updateReservation);
+router.delete('/reservations/:reservationId', validateMongoId('reservationId'), checkValidation, reservationController.deleteReservation);
 
 export default router;
