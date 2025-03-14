b6abb581e6a9d7d617092306780d21a4426af2f2
rovmont <rovin_montano@dlsu.edu.ph>
2025-03-15 00:20:23 +0800

    feature: about us page style: fixed reserve slot chore: modified routes a bit

--- routes/homepageRoutes.js
+++ routes/homepageRoutes.js
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).render('index', { title: 'LabKita!' });
+    res.status(200).render('index', {
+        title: 'LabKita!'
+    });
 });
 
 export default router;
--- routes/manageReservationsRoutes.js
+++ routes/manageReservationsRoutes.js
@@ -4,14 +4,15 @@ import { manageReservationController } from '../controllers/controllers.js';
 const router = express.Router();
 
 router.get('/manage-reservations', (req, res) => {
-    res.render('manage-reservations', { title: "Manage Reservations" });
+    res.render('manage-reservations', {
+        title: "Manage Reservations"});
 });
 
 router.get('/manage-reservations/edit', manageReservationController.getEditReservationPage);
 
 router.get('/manage-reservations/create', (req, res) => {
     res.render('reserve-slot', { 
-        title: "Reserve Slot"
+        title: "Reserve Slot",
     });
 });
 
