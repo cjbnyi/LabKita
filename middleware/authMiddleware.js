6efa9b99a81e69f913ecf330a6e0e0e45aaa54db
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2025-04-01 04:01:02 +0800

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -15,11 +15,21 @@ const authorizeRole = (...roles) => (req, res, next) => {
 const authenticateToken = (req, res, next) => {
     const token = req.cookies.accessToken;
 
-    if (!token) return res.status(401).json({ message: 'Access denied' });
+    console.log("Received authentication request.");
+    console.log("Access Token from cookies:", token);
+
+    if (!token) {
+        console.warn("No access token found.");
+        return res.status(401).json({ message: 'Access denied' });
+    }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.status(403).json({ message: 'Invalid token' });
+        if (err) {
+            console.error("Token verification failed:", err);
+            return res.status(403).json({ message: 'Invalid token' });
+        }
 
+        console.log("Token verified successfully. User data:", user);
         req.user = user;
         next();
     });
