771c099a54a29f13e17ebb0652c1dbabc12121e9
rovmont <rovin_montano@dlsu.edu.ph>
2025-04-07 04:13:56 +0800

    feat: incorporated dependencies and npm packages style: edited layout for more aesthetically pleasing appearance chore: fixed connections with css file for proper rendering

--- controllers/non-entity/homepageController.js
+++ controllers/non-entity/homepageController.js
@@ -1,12 +1,15 @@
 const renderIndex = (req, res) => {
-    res.status(200).render('index', { 
+    res.status(200).render('index', {
         title : 'LabKita!',
         user : res.locals.user
     });
 }
 
 const renderAboutUs = (req, res) => {
-    res.status(200).render('about-us', { title : 'LabKita!' });
+    res.status(200).render('about-us', {
+        title : 'LabKita!',
+        aboutusCSS : '/public/css/about-us.css'
+    });
 }
 
 export default {
